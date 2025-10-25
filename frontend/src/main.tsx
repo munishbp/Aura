c2ca9fe92a6d13cfffc404ae02ea3f3a1f3e941d
Sahif05 <106846355+Sahif05@users.noreply.github.com>
2025-10-25 13:08:27 -0400

    tthis is a working router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
+// src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // 1. Imported
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* 2. You MUST wrap <App /> here */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
